1b8d1ab816205d983f501d4bec5e914b0bb7f363
Ayush Sharma <ed.ayush2003@gmail.com>
2023-07-15 18:19:16 +0000

    Updates in project

--- frost-head/src/utils/links.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Links(props){
-    return (<a href={props.link} className='px-4 py-2 hover:bg-white hover:text-black transition-all duration-150 cursor-pointer '>
-        {props.text}
-    </a>);
-}
-
-export default Links;
